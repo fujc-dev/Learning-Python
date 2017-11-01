750d48459688c37ada39dc5cd41d4186efb74a52
dane55 <328969111@qq.com>
2017-11-01 17:10:23 +0800

    Learning-Python

--- zhihuCrawl/zhihuCrawl/spiders/zhihu_com.py
+++ zhihuCrawl/zhihuCrawl/spiders/zhihu_com.py
@@ -8,14 +8,35 @@ class ZhihuComSpider(CrawlSpider):
     name = 'zhihu.com'
     allowed_domains = ['zhihu.com']
     start_urls = ['http://zhihu.com/']
+    # 模拟浏览器
+    request_header = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Connection": "keep-alive",
+        "Content-Length": 137,
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Host": "www.zhihu.com",
+        "Referer": "https://www.zhihu.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "X-Requested-With": "X-Requested-With",
+        "X-Xsrftoken": "d098c6f7-c359-41e3-99c6-16735f9dc20e"
+    }
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
+    def start_requests(self):
+        '''
+
+        :return:
+        '''
+        pass
+
     def parse_item(self, response):
         i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        # i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        # i['name'] = response.xpath('//div[@id="name"]').extract()
+        # i['description'] = response.xpath('//div[@id="description"]').extract()
         return i
